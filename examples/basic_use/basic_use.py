017ac180fcfac1999474b92af7676a21e217be33
Julian <holland@fhi.mpg.de>
2025-08-12 10:46:09 +0200

    ruff reformat

--- examples/basic_use/basic_use.py
+++ examples/basic_use/basic_use.py
@@ -4,12 +4,9 @@ from pathlib import Path
 from alomancy.core.standard_active_learning import ActiveLearningStandardMACE
 
 al_workflow = ActiveLearningStandardMACE(
-    initial_train_file_path=Path(
-        "structure_data/ac_all_33_2025_07_11_ftrim_100_train_set.xyz"
-    ),
-    initial_test_file_path=Path(
-        "structure_data/ac_all_33_2025_07_11_ftrim_100_test_set.xyz"
-    ),
+    initial_train_file_path=Path("input_files/C_Na_amorphous_5255_train.xyz"),
+    initial_test_file_path=Path("input_files/C_Na_amorphous_583_test.xyz"),
+    config_file_path=Path("input_files/standard_config.yaml"),
     number_of_al_loops=25,
     verbose=1,
     start_loop=0,
