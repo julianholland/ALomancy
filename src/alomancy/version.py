ff0d3c44468738f8bea6a78ee83fbefb5187afae
Julian <holland@fhi.mpg.de>
2025-08-12 13:00:36 +0200

    readme updates

--- src/alomancy/version.py
+++ src/alomancy/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev0+g85bb7023c.d20250812'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev0', 'g85bb7023c.d20250812')
+__version__ = version = '0.1.0.post0'
+__version_tuple__ = version_tuple = (0, 1, 0, 'post0')
 
-__commit_id__ = commit_id = 'g85bb7023c'
+__commit_id__ = commit_id = 'gcd648e316'
